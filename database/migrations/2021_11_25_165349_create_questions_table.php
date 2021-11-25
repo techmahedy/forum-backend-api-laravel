478cf3bc20ed660239255972692efc849ac63bbc
techmahedy <mahedy150101@gmail.com>
2021-11-25 23:14:37 +0600

    migration added

---
+++ database/migrations/2021_11_25_165349_create_questions_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->index()
+                ->constrained()
+                ->onDelete('cascade');
+            $table->foreignId('category_id')
+                ->index()
+                ->constrained()
+                ->onDelete('cascade');
+            $table->string('question_title')->unique();
+            $table->string('question_slug')->unique();
+            $table->tinyText('question_body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
